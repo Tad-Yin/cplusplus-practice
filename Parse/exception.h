c50ed1fbbf168f2ce85d6f97f7e9ae8825a24893
Tad-Yin <yinkejie0913@163.com>
2017-07-30 19:10:01 +0800

    change to cplusplus

--- Parse/exception.h
+++ Parse/exception.h
@@ -2,10 +2,10 @@
 #define __EXCEPTION_H__
 
 struct StrException {
-	StrException(const char* _aText, const char* _aError)
+	explicit StrException(const char* _aText, const char* _aError)
 		:aText(_aText),aError(_aError) {}
 	const char* aText;
-    const char* aError;
+        const char* aError;
 };
 
 #endif /* ifndef __EXCEPTION_H__ */
